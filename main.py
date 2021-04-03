e28d9b5ee82a030c141d4550e3a9f5134c19653a
lev <60739000+WeezerIrony@users.noreply.github.com>
2021-04-03 20:54:17 +0700

    the agame
    works now

--- main.py
+++ main.py
@@ -5,43 +5,44 @@ cross_id = 1
 nought_id = 2
 
 
-def gamestat(f: dict):
-    for j in range(1, 4):
-        print(f[j][1] + f[j][2] + f[j][3])
+def gamestat(f: list):
+    for j in range(3):
+        print(f[j][0] + f[j][1] + f[j][2])
 
 
-def makemove(f: dict, player: int):
+def makemove(f: list, player: int):
     while True:
         try:
-            space = int(input("SELECT SPACE (1-9): "))
-            if space in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
-                row = (space - 1) // 3
-                column = (space - 1) % 3
-                if f[row][column] == '-':
-                    f[row][column] = 'X' if player == 1 else 'O'
-                    break
-                else:
-                    print("[SPACE ALREADY OCCUPIED]")
-            else:
-                print("[BAD INPUT]")
+            space = int(input("SELECT SPACE (1-9): "))  # except no longer in popa
         except:
+            print("[BAD INPUT]")
+        if space in range(1, 10):
+            row = (space - 1) // 3
+            column = (space - 1) % 3
+            if f[row][column] == '-':
+                f[row][column] = 'X' if player == 1 else 'O'
+                break
+            else:
+                print("[SPACE ALREADY OCCUPIED]")
+        else:
             print("[BAD INPUT]")
 
 
-def checkwin(f: dict):
-    for k in range(1, 4):
-        if f[k][1] == f[k][2] == f[k][3] != '-':
-            return cross_id if f[k][1] == "X" else nought_id
-        elif f[1][k] == f[2][k] == f[3][k] != '-':
-            return cross_id if f[1][k] == "X" else nought_id
-    if f[1][1] == f[2][2] == f[3][3] != '-':
-        return cross_id if f[1][1] == "X" else nought_id
-    elif f[1][3] == f[2][2] == f[3][1] != '-':
-        return cross_id if f[1][3] == "X" else nought_id
-
+def checkwin(f: list):
+    for k in range(3):
+        if f[k][0] == f[k][1] == f[k][2] != '-':
+            return cross_id if f[k][0] == "X" else nought_id
+        elif f[0][k] == f[1][k] == f[2][k] != '-':
+            return cross_id if f[0][k] == "X" else nought_id
+    if f[0][0] == f[1][1] == f[2][2] != '-':
+        return cross_id if f[0][0] == "X" else nought_id
+    elif f[0][2] == f[1][1] == f[2][0] != '-':
+        return cross_id if f[0][2] == "X" else nought_id
+    else:
+        return 0
 
 def main():
-    field = {1: {1: '-', 2: '-', 3: '-'}, 2: {1: '-', 2: '-', 3: '-'}, 3: {1: '-', 2: '-', 3: '-'}}
+    field = [['-','-','-'],['-','-','-'],['-','-','-']]
     winner_id = 0
     os.system('cls')
     for move in range(1, 10):
@@ -61,6 +62,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
